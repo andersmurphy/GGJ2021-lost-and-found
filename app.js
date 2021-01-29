c94a8225ee274eee843e603aa380b95cccaa0204
andersmurphy <andersmurphy@gmail.com>
2021-01-29 20:08:29 +0000

    Planet?

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const app = new PIXI.Application()
 document.body.appendChild(app.view)
 
 // load the texture we need
-app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture)
+app.loader.add('planet', 'planet.png').load((loader, resources) => {
+    // This creates a texture from a 'planet.png' image
+    const planet = new PIXI.Sprite(resources.planet.texture)
 
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2
-    bunny.y = app.renderer.height / 2
+    // Setup the position of the planet
+    planet.x = app.renderer.width / 2
+    planet.y = app.renderer.height / 2
 
     // Rotate around the center
-    bunny.anchor.x = 0.5
-    bunny.anchor.y = 0.5
+    planet.anchor.x = 0.5
+    planet.anchor.y = 0.5
 
-    // Add the bunny to the scene we are building
-    app.stage.addChild(bunny)
+    // Add the planet to the scene we are building
+    app.stage.addChild(planet)
 
     // Listen for frame updates
     app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01
+         // each frame we spin the planet around a bit
+        planet.rotation += 0.01
     })
 })
