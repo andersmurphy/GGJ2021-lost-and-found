385d0d82ed625831f73b6b28c1bd0bf862eb579e
jockf <jockfindlay@gmail.com>
2021-01-29 22:32:44 +0000

    A sample ink script to test. Adding a placeholder dialog background. Notes on how Inkle should work - take the guts out of main.js and update on a tick function, put content on to dialog container. WIP

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
 const app = new PIXI.Application()
+var didMeetSnake = false
+//const loader = PIXI.Loader.shared;
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
 document.body.appendChild(app.view)
 
 // load the texture we need
-app.loader.add('planet', 'planet.png').load((loader, resources) => {
+app.loader
+    .add('planet', 'planet.png')
+    .add('DialogBackground', 'DialogBackground.png')
+    .load((loader, resources) => {
   // This creates a texture from a 'planet.png' image
+  //dialogBackground = new PIXI.Sprite(resources.DialogBackground.texture)
   const planet = new PIXI.Sprite(resources.planet.texture)
   planet.width = 200
   planet.height = 200
@@ -30,4 +36,24 @@ app.loader.add('planet', 'planet.png').load((loader, resources) => {
     // each frame we spin the planet around a bit
     planet.rotation += 0.01
   })
+  app.ticker.add(() => {
+      if (!didMeetSnake) {
+          didMeetSnake = true
+          showSnakeDialog()
+      }
+  })
 })
+
+function showSnakeDialog() {
+    // add dialog box beside prince
+    let background = new PIXI.Sprite(app.loader.resources.DialogBackground.texture)
+
+    background.width = 600
+    background.height = 300
+    background.x = 5
+    background.y = 5
+
+    app.stage.addChild(background)
+    // run ink script 
+}
+// Desired: When the prince collides with another character a dialog appears
