a6498054a9ea7f19d75f07b346f99076c523f396
andersmurphy <andersmurphy@gmail.com>
2021-01-29 20:17:37 +0000

    Make planet smaller

--- app.js
+++ app.js
@@ -9,23 +9,25 @@ document.body.appendChild(app.view)
 
 // load the texture we need
 app.loader.add('planet', 'planet.png').load((loader, resources) => {
-    // This creates a texture from a 'planet.png' image
-    const planet = new PIXI.Sprite(resources.planet.texture)
+  // This creates a texture from a 'planet.png' image
+  const planet = new PIXI.Sprite(resources.planet.texture)
+  planet.width = 200
+  planet.height = 200
 
-    // Setup the position of the planet
-    planet.x = app.renderer.width / 2
-    planet.y = app.renderer.height / 2
+  // Setup the position of the planet
+  planet.x = app.renderer.width / 2
+  planet.y = app.renderer.height / 2
 
-    // Rotate around the center
-    planet.anchor.x = 0.5
-    planet.anchor.y = 0.5
+  // Rotate around the center
+  planet.anchor.x = 0.5
+  planet.anchor.y = 0.5
 
-    // Add the planet to the scene we are building
-    app.stage.addChild(planet)
+  // Add the planet to the scene we are building
+  app.stage.addChild(planet)
 
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the planet around a bit
-        planet.rotation += 0.01
-    })
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the planet around a bit
+    planet.rotation += 0.01
+  })
 })
