4795ba38af40922ec7665ffdf068f4f30a634145
andersmurphy <andersmurphy@gmail.com>
2021-01-30 12:23:06 +0000

    Set up prince sprite

--- app.js
+++ app.js
@@ -4,95 +4,109 @@
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
 const app = new PIXI.Application(
-  { width: 1280,
+  {
+    width: 1280,
     height: 720,
     antialias: true,
   })
-var didMeetSnake = true
-  
+let didMeetSnake = false
+
 // The application will create a canvas element for you that you
 // can then insert into the DOM
 document.body.appendChild(app.view)
 
-// load the texture we need
 app.loader
-    .add('planet', 'planet.png')
-    .add('DialogBackground', 'DialogBackground.png')
-    .load((loader, resources) => {
-  // This creates a texture from a 'planet.png' image
-  //dialogBackground = new PIXI.Sprite(resources.DialogBackground.texture)
-  const planet = new PIXI.Sprite(resources.planet.texture)
-  planet.width = 1000
-  planet.height = 1000
+  .add('planet', 'planet.png')
+  .add('DialogBackground', 'DialogBackground.png')
+  .load((loader, resources) => {
 
-  // Setup the position of the planet
-  planet.x = app.renderer.width / 2
-  planet.y = app.renderer.height + app.renderer.height / 3
+    // Planet
+    const planet = new PIXI.Sprite(resources.planet.texture)
+    planet.width = 1000
+    planet.height = 1000
+    planet.x = app.renderer.width / 2
+    planet.y = app.renderer.height + 300
+    planet.anchor.x = 0.5
+    planet.anchor.y = 0.5
+    app.stage.addChild(planet)
 
-  // Rotate around the center
-  planet.anchor.x = 0.5
-  planet.anchor.y = 0.5
+    // Prince
+    const prince = new PIXI.Sprite(resources.planet.texture)
+    prince.width = 100
+    prince.height = 100
+    prince.x = app.renderer.width / 2
+    prince.y = app.renderer.height - 250
+    prince.anchor.x = 0.5
+    prince.anchor.y = 0.5
+    app.stage.addChild(prince)
 
-  // Add the planet to the scene we are building
-  app.stage.addChild(planet)
 
-  let state = { planetRotation: 0 }
-
-  app.ticker.add(
-    () => {
-      planet.rotation += state.planetRotation
-    })
-
-  const upListener = event => {
-    switch (event.key) {
-      case "a":
-        state.planetRotation = 0
-        break;
-      case "d":
-        state.planetRotation = 0
-        break;
-      default:
+    let state =
+    {
+      planetRotation: 0,
+      princeRotation: 0
     }
-  }
 
-  const downListener = event => {
-    switch (event.key) {
-      case "a":
-        state.planetRotation = 0.05
-        break;
-      case "d":
-        state.planetRotation = -0.05
-        break;
-      default:
+    app.ticker.add(
+      () => {
+        planet.rotation += state.planetRotation
+        prince.rotation += state.princeRotation
+      })
+
+    const upListener = event => {
+      switch (event.key) {
+        case "a":
+          state.planetRotation = 0
+          state.princeRotation = 0
+          break;
+        case "d":
+          state.planetRotation = 0
+          state.princeRotation = 0
+          break;
+        default:
+      }
     }
-  }
 
-  document.addEventListener("keydown", downListener, false)
-  document.addEventListener("keyup", upListener, false)
+    const downListener = event => {
+      switch (event.key) {
+        case "a":
+          state.planetRotation = 0.05
+          state.princeRotation = -0.05
+          break;
+        case "d":
+          state.planetRotation = -0.05
+          state.princeRotation = 0.05
+          break;
+        default:
+      }
+    }
 
-  const showSnakeDialog = () => {
-    // add dialog box beside prince
-    let container = new PIXI.Container();
-    let background = new PIXI.Sprite(app.loader.resources.DialogBackground.texture)
+    document.addEventListener("keydown", downListener, false)
+    document.addEventListener("keyup", upListener, false)
 
-    container.width = 600
-    container.height = 300
-    container.x = 8
-    container.y = 8
+    const showSnakeDialog = () => {
+      // add dialog box beside prince
+      let container = new PIXI.Container();
+      let background = new PIXI.Sprite(app.loader.resources.DialogBackground.texture)
 
-    container.addChild(background);
+      container.width = 600
+      container.height = 300
+      container.x = 8
+      container.y = 8
 
-    app.stage.addChild(container)
-    // run ink script 
-    loadStory(helloWorldStoryContent, container)
-  }
+      container.addChild(background);
 
-  const interactionListener = _ => {
-    if (!didMeetSnake) { // Faking it: When the prince interacts with another character a dialog appears
+      app.stage.addChild(container)
+      // run ink script
+      loadStory(helloWorldStoryContent, container)
+    }
+
+    const interactionListener = _ => {
+      if (!didMeetSnake) { // Faking it: When the prince interacts with another character a dialog appears
         didMeetSnake = true
         showSnakeDialog()
+      }
     }
-  }
 
-  app.ticker.add(interactionListener)
-})
+    app.ticker.add(interactionListener)
+  })
